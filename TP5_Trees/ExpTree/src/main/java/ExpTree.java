f6e10014b4e59aa726074bfa491bb34f20578a55
Bautista Pessagno <bpessagno@itba.edu.ar>
2025-05-14 13:26:03 -0300

    BinaryTree y BinarySearhTree

---
+++ TP5_Trees/ExpTree/src/main/java/ExpTree.java
@@ -0,0 +1,185 @@
+import java.util.Scanner;
+
+public class ExpTree implements ExpressionService {
+
+	private Node root;
+
+	public ExpTree() {
+	    System.out.print("Introduzca la expresi�n en notaci�n infija con todos los par�ntesis y blancos: ");
+
+		// token analyzer
+	    Scanner inputScanner = new Scanner(System.in).useDelimiter("\\n");
+	    String line= inputScanner.nextLine();
+	    inputScanner.close();
+
+	    buildTree(line);
+	}
+	
+	private void buildTree(String line) {	
+		  // space separator among tokens
+		  Scanner lineScanner = new Scanner(line).useDelimiter("\\s+");
+		  root= new Node(lineScanner);
+		  lineScanner.close();
+		  printTree(root);
+	}
+
+	@Override
+	public double eval(){
+		return root.eval();
+	}
+
+	@Override
+	public void preorder() {
+		root.preOrdeer();
+	}
+
+	@Override
+	public void inorder() {
+		root.inOrdeer();
+	}
+
+	@Override
+	public void postorder() {
+		root.postOrdeer();
+	}
+
+	private void printTree(Node node) {
+		System.out.println("Inorder:");
+		inorder();
+		System.out.println("\nPreorder:");
+		preorder();
+		System.out.println("\nPostorder:");
+		postorder();
+		System.out.println("\nEval: " + eval());
+	}
+	
+
+
+	
+	static final class Node {
+		private String data;
+		private Node left, right;
+		
+		private Scanner lineScanner;
+
+		public Node(Scanner theLineScanner) {
+			lineScanner= theLineScanner;
+			
+			Node auxi = buildExpression();
+			data= auxi.data;
+			left= auxi.left;
+			right= auxi.right;
+			
+			if (lineScanner.hasNext() ) 
+				throw new RuntimeException("Bad expression");
+		}
+
+		public void inOrdeer() {
+			System.out.print(data + " ");
+			if (left != null) {
+				left.inOrdeer();
+			}
+			if (right != null) {
+				right.inOrdeer();
+			}
+		}
+
+		public void preOrdeer() {
+			if (left != null) {
+				System.out.printf("( ");
+				left.preOrdeer();
+			}
+			System.out.print(data + " ");
+			if (right != null) {
+				right.preOrdeer();
+				System.out.printf(") ");
+			}
+		}
+
+		public void postOrdeer() {
+			if (left != null) {
+				left.postOrdeer();
+			}
+			if (right != null) {
+				right.postOrdeer();
+			}
+			System.out.print(data + " ");
+		}
+		
+		private Node() {
+		}
+		
+	
+		private Node buildExpression() {
+			// caso 1: E->(E op E) siendo op +, -, *, /
+			// caso ejemplo ( a + ( b * c ) )
+			// ahora el lineScanner apunta a (
+			if (lineScanner.hasNext("\\(")) { // como es ( entra
+				lineScanner.next();	 //lo salteo
+				Node left= buildExpression(); // llama a la izquierda con el line scanner en a -> va a retornar a terminando el lineScanner en +
+
+				if (!lineScanner.hasNext()) { // si no hay un + es porque no hay un operador
+					throw new RuntimeException("missing or invalid operator");
+				}
+				String op= lineScanner.next();	// se guarda el operador
+				if(!Utils.isOperator(op))
+					throw new RuntimeException("missing or invalid operator");
+
+				Node right= buildExpression(); // llama a la derecha con el line scanner estando en ( va a volver a hacer todo para ( b * c )
+				if(!lineScanner.hasNext("\\)")) { // si no hay un ) es porque no hay un operador
+					throw new RuntimeException("missing");
+				}
+				lineScanner.next(); // consume )
+
+				Node node= new Node();
+				node.data= op;
+				node.left= left;
+				node.right= right;
+
+				return node;
+			}
+			// caso 2: E->cte
+			Node node = new Node();
+			if (!lineScanner.hasNext()) {
+				throw new RuntimeException("Bad expression");
+			}
+			node.data= lineScanner.next();
+			if(!Utils.isConstant(node.data)) {
+				throw new RuntimeException("Bad expression");
+			}
+			node.left= null;
+			node.right= null;
+			return node;
+		}
+
+		private double eval(){
+			if(left == null && right == null) {
+				return Utils.getDoubleConstant(data);
+			}
+			return operate(data, left.eval(), right.eval());
+		}
+
+		private double operate(String data, double left, double right) {
+			switch (data){
+				case "+": return left + right;
+				case "-": return left - right;
+				case "*": return left * right;
+				case "/": return left / right;
+				case "^": return Math.pow(left, right);
+			}
+			throw new RuntimeException("wrong operator");
+		}
+
+
+
+	}  // end Node class
+
+	
+	
+	// hasta que armen los testeos
+	public static void main(String[] args) {
+		ExpressionService myExp = new ExpTree();
+	
+	}
+
+}  // end ExpTree class
---
+++ TP5_Trees/TP_5B/BSTree/src/main/java/Node.java
@@ -0,0 +1,125 @@
+public class Node<T extends Comparable<? super T>> implements NodeTreeInterface<T>{
+
+    private T data;
+    private Node left;
+    private Node right;
+    //Para AVL
+    private int height;
+
+    public Node(T data) {
+        this.data = data;
+        this.right = null;
+        this.left = null;
+    }
+
+    @Override
+    public T getData(){
+        return data;
+    }
+
+    @Override
+    public NodeTreeInterface<T> getLeft(){
+        return left;
+    }
+
+    @Override
+    public NodeTreeInterface<T> getRight(){
+        return right;
+    }
+
+    public void insert(T myData) {
+        if(myData == null)
+            throw new IllegalArgumentException("Null data");
+        if(myData.compareTo(data) <= 0) {
+            if(left == null)
+                left = new Node(myData);
+            else
+                left.insert(myData);
+        } else {
+            if(right == null)
+                right = new Node(myData);
+            else
+                right.insert(myData);
+        }
+    }
+
+    public String preorder(StringBuilder s) {
+        s.append(data).append(" ");
+        if(left != null)
+            left.preorder(s);
+        if(right != null)
+            right.preorder(s);
+        return s.toString();
+    }
+
+    public String postorder(StringBuilder s) {
+        if(left != null)
+            left.preorder(s);
+        if(right != null)
+            right.preorder(s);
+        s.append(data).append(" ");
+        return s.toString();
+    }
+
+    public String inorder(StringBuilder s) {
+        if(left != null)
+            left.preorder(s);
+        s.append(data).append(" ");
+        if(right != null)
+            right.preorder(s);
+        return s.toString();
+    }
+
+    public int getHeight() {
+        if (left == null && right == null)
+            return 0;
+        else {
+            int leftHeight = (left == null ? 0 : left.getHeight());
+            int rightHeight = (right == null ? 0 : right.getHeight());
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public boolean contains(T myData){
+        int c = data.compareTo(myData);
+        if(c == 0)
+            return true;
+        if(left != null && c > 0)
+            return left.contains(myData);
+        if(right!=null && c < 0)
+            return right.contains(myData);
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Node<T> delete(T myData){
+        if(myData.compareTo(data) < 0){
+            if(left != null)
+                left = left.delete(myData);
+        }
+        if(myData.compareTo(data) > 0){
+            if(right != null)
+                right = right.delete(myData);
+        }
+        else {
+            if(left == null)
+                return (Node<T>) right;
+            if(right == null)
+                return (Node<T>) left;
+            data = (T)right.findlowest();
+            right = right.delete(data);
+            return this;
+        }
+
+        return this;
+    }
+
+    public T findlowest(){
+        Node<T> current = this;
+        while(current.left != null){
+            current = current.left;
+        }
+        return current.data;
+    }
+
+}
